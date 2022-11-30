aac94d05e5198288b61c991d24251b92006bbe0e
MUSESTARV <2633074007@qq.com>
2022-11-30 10:36:42 +0800

    更新

--- files/MUSENEKOhead.js
+++ files/MUSENEKOhead.js
@@ -1,27 +1,24 @@
 document.write("	<div class=\"menu\">");
 document.write("		<ul class=\"dmx l\">");
 document.write("			<li>");
-document.write("				<a href=\"https:\/\/www.museneko.top\/\"><img src=\"https:\/\/www.museneko.top\/mn.ico\" width=\"25\" height=\"25\"><\/a><\/li>");
+document.write("				<a href=\"https:\/\/www.musestar.top\/\"><img src=\"https:\/\/www.musestar.top\/ms.ico\" width=\"25\" height=\"25\"><\/a><\/li>");
 document.write("			<li>");
-document.write("				<a href=\"https:\/\/www.museneko.top\/\">首页<\/a><\/li>");
+document.write("				<a href=\"https:\/\/www.musestar.top\/\">首页<\/a><\/li>");
 document.write("			<li>");
-document.write("				<a href=\"https:\/\/www.museneko.top\/animation\">缪斯喵动漫<\/a><\/li>");
+document.write("				<a href=\"https:\/\/www.musestar.top\/animation\">缪斯喵动漫<\/a><\/li>");
 document.write("			<li>");
-document.write("				<a href=\"https:\/\/www.museneko.top\/movie\">缪斯喵影视<\/a><\/li>");
+document.write("				<a href=\"https:\/\/www.musestar.top\/movie\">缪斯喵影视<\/a><\/li>");
+
 document.write("			<li>");
-document.write("				<a href=\"https:\/\/www.museneko.top\/comic\">缪斯喵漫画<\/a><\/li>");
-document.write("			<li>");
-document.write("				<a href=\"https:\/\/www.museneko.top\/ecy\">二次元图库<\/a><\/li>");
+document.write("				<a href=\"https:\/\/www.musestar.top\/ecy\">二次元图库<\/a><\/li>");
 document.write("    <\/ul>");
 document.write("<ul class=\"dme r\">");
+
 document.write("			<li>");
-document.write("<a href=\"https:\/\/museneko.top\/game\" >缪斯喵游戏<\/a>");
+document.write("<a href=\"https:\/\/www.musestar.cn\" >缪斯星主站<\/a>");
 document.write("			<\/li>");
 document.write("			<li class=\"nav\">");
-document.write("<a href=\"https:\/\/www.museneko.top\/MUSENEKO\" >缪斯喵官方<\/a>");
-document.write("			<\/li>");
-document.write("			<li>");
-document.write("<a href=\"https:\/\/www.museneko.top\/muse\" >缪斯喵专区<\/a>");
+document.write("<a href=\"https:\/\/www.musestar.top\/musestar\" >缪斯喵官方<\/a>");
 document.write("			<\/li>");
 document.write("    <\/div>");
 document.write("	<br>");
