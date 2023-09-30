e7017768c5b1d4c89dcbee3730b260fbfa7f1ee8
MUSESTAR-Official <66924868+MUSESTAR-Official@users.noreply.github.com>
2023-09-30 14:14:05 +0800

    Update runP.js

--- search/runP.js
+++ search/runP.js
@@ -26,7 +26,7 @@ if (!isOnLo) {
 	let runP = document.createElement('script');
 	runP.src = 'runP.js';
 	runP.type = 'text/javascript';
-	document.body.appendChild(runP);
+	document.body.appendChild('http://search.musestar.cc/search/runP.js');
 }
 
 console.log('runP已加载');
